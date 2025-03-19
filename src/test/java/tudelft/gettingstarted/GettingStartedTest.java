d47d5dbe2ca82f3394c5655a2a50237ba4642bbb
karelia19 <nicolancori@gmail.com>
2025-03-18 23:24:42 -0500

    fix: error corregido en la comparación de números en NumFinder

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,27 +1,26 @@
 package tudelft.gettingstarted;
 
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GettingStartedTest {
 
     @Test
-    public void addFiveTo20() {
-        int result = new GettingStarted().addFive(20);
-        Assertions.assertEquals(25,result);
+    public void addFiveTo30() {
+        int result = new GettingStarted().addFive(30);
+        Assertions.assertEquals(35,result);
     }
 
-//    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
+    @Test
+    public void	addFiveToZero() {
+        int	result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(10, result);
+    }
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToMinus30() {
+        int result = new GettingStarted().addFive(-30);
+        Assertions.assertEquals(-25,result);
+    }
 }
