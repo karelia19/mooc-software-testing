1a2d8383a3ab2f3c120420bd790fca6adaff5213
karelia19 <nicolancori@gmail.com>
2025-03-28 16:54:17 -0500

    fix: error corregido en gHappy, para evitar el mensaje IndexOutOfBoundsException y agregue test.

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,39 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GHappyTest {
+
+    @Test
+    public void testGGEnMedio() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("xxggxx")); // verdadero
+    }
+
+    @Test
+    public void testGUnica() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("xxgxx")); // falso
+    }
+
+
+    @Test
+    public void testSoloGsFelices() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("gggg")); // verdadero
+    }
+
+    @Test
+    public void testCadenaVacia() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("")); // verdadero
+    }
+
+    @Test
+    public void testGAlInicioYAlFinal() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("gxxg")); // falso
+    }
 }
+
