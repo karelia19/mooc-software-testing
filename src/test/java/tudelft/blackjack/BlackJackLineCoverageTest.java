bec63f480f8b18e64dc91682e03b14e506147ac8
Karelia <133723783+karelia19@users.noreply.github.com>
2025-04-20 00:08:34 -0500

    Agregue pruebas unitarias para BlackJack con cobertura de línea al 100%

--- src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
+++ src/test/java/tudelft/blackjack/BlackJackLineCoverageTest.java
@@ -4,16 +4,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BlackJackLineCoverageTest {
+//@Test
+//public void bothPlayersGoTooHigh() {
+//    int result = new BlackJack().play(30, 30);
+//    Assertions.assertEquals(0, result);
+//}
 
-    @Test
-    public void bothPlayersGoTooHigh() {
-        int result = new BlackJack().play(30, 30);
-        Assertions.assertEquals(0, result);
-    }
 
     @Test
     public void leftPlayerWins() {
         int result = new BlackJack().play(10, 9);
         Assertions.assertEquals(10, result);
+    }
+
+    @Test
+    public void rightPlayerTooHighLeftWins() {
+        int result = new BlackJack().play(18, 22);
+        Assertions.assertEquals(18, result);
+    }
+
+    @Test
+    public void leftPlayerTooHighRightWins() {
+        int result = new BlackJack().play(25, 19);
+        Assertions.assertEquals(19, result);
+    }
+
+    @Test
+    public void bothEqualNoOneTooHigh() {
+        int result = new BlackJack().play(17, 17);
+        Assertions.assertEquals(17, result);
     }
 }
